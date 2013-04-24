e7421485cc50e84f86e36ad8f7f79f49b94df816
David Gullmarsvik <david.gullmarsvik@gmail.com>
2013-04-24 07:39:28 +0200

    Example 3: Asserts

--- NUnitDemo.Tests/LogAnalyzerTests.cs
+++ NUnitDemo.Tests/LogAnalyzerTests.cs
@@ -11,7 +11,20 @@ namespace NUnitDemo.Tests
     public class LogAnalyzerTests
     {
         [Test]
-        public void IsValidFileName_ValidName_ReturnTrue()
+        public void IsValidFileName_ValidNameLower_ReturnTrue()
+        {
+            // Arrange
+            var analyzer = new LogAnalyzer();
+
+            // Act
+            var actual = analyzer.IsValidFileName("testfile.slf");
+
+            // Assert
+            Assert.That(actual);
+        }
+
+        [Test]
+        public void IsValidFileName_ValidNameUpper_ReturnTrue()
         {
             // Arrange
             var analyzer = new LogAnalyzer();
@@ -21,6 +34,70 @@ namespace NUnitDemo.Tests
 
             // Assert
             Assert.That(actual);
+        }
+
+        [Test]
+        public void CurrentVersion_v10_returnsv10()
+        {
+            // Arrange
+            var analyzer = new LogAnalyzer();
+            var expected = "v1.0";
+
+            // Act
+            var actual = analyzer.CurrentVersion;
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CheckConfigFile_NonExistingFile_ShouldThrow()
+        {
+            // Arrange
+            var analyzer = new LogAnalyzer();
+
+            // Assert
+            Assert.Throws(typeof(ArgumentException), analyzer.CheckConfigFile);
+        }
+
+        [Test]
+        public void CheckConfigFile_EmptyStringFileName_ShouldThrow()
+        {
+            // Arrange
+            var analyzer = new LogAnalyzer();
+            var configFile = "";
+            var version = "v1.0";
+
+            // Assert
+            Assert.Throws(Is.TypeOf<ArgumentException>(),
+                            delegate { 
+                                analyzer.CheckConfigFile(configFile, version); });
+        }
+
+        [Test]
+        public void GenerateLogObject_ValidLog_ShoulBeOfTypeLogObject()
+        {
+            // Arrange
+            var analyzer = new LogAnalyzer();
+
+            // Act
+            var actual = analyzer.GenerateLogObject();
+
+            // Assert
+            Assert.That(actual, Is.TypeOf(typeof(DemoLogObject)));
+        }
+
+        [Test]
+        public void GenerateLogObject_ValidLog_NotNull()
+        {
+            // Arrange
+            var analyzer = new LogAnalyzer();
+
+            // Act
+            var actual = analyzer.GenerateLogObject();
+
+            // Assert
+            Assert.That(actual, Is.Not.Null);
         }
     }
 }
--- NUnitDemo/LogAnalyzer.cs
+++ NUnitDemo/LogAnalyzer.cs
@@ -10,11 +10,38 @@ namespace NUnitDemo
     {
         public bool IsValidFileName(string fileName)
         {
-            if (!fileName.EndsWith(".SLF"))
+            if (!fileName.ToLower().EndsWith(".slf"))
             {
                 return false;
             }
             return true;
         }
+
+        public string CurrentVersion { get { return "v1.0"; } }
+
+        public void CheckConfigFile()
+        {
+            if (!ConfigFileExists)
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public void CheckConfigFile(string configFile, string version = "v1.0")
+        {
+            if (string.IsNullOrEmpty(configFile))
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public DemoLogObject GenerateLogObject() 
+        { 
+            return new DemoLogObject(); 
+        }
+
+        private bool ConfigFileExists { get { return false; } }
     }
+
+    public class DemoLogObject { }
 }
