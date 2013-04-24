c999b31f97a18e833dbe950a54ebd12505e6425e
David Gullmarsvik <david.gullmarsvik@gmail.com>
2013-04-24 06:27:53 +0200

    Example 1: First Test

---
+++ NUnitDemo.Tests/LogAnalyzerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitDemo.Tests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void first_test()
+        {
+            Assert.That(2 + 2, Is.EqualTo(4));
+        }
+    }
+}
